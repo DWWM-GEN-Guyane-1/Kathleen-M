1060758d3406c45ca4effe0eef60670e70abdacd
Kathleen_M <kathleen.malialin@gmail.com>
2019-12-27 23:33:19 -0300

    add-corr

--- site-animations/script.js
+++ site-animations/script.js
@@ -1,22 +1,18 @@
-(document).ready(function() {
+
+     $(document).ready(function(){
+     var reponse = prompt("Bonjour, comment t'appelles-tu ?", "<Entrez ici votre prénom>");
+     if (reponse == null) {
+       alert("Vous avez cliqué sur Annuler");
+     } else {
+       alert("Bienvenue " + reponse + ", ça roule ?");
+     }
+
     $('.js-scrollTo').on('click', function() { // Au clic sur un élément
         var page = $(this).attr('href'); // Page cible
         var speed = 1800; // Durée de l'animation (en ms)
         $('html, body').animate( { scrollTop: $(page).offset().top }, speed ); // Go
         return false;
     });
-});
 
-$(function() {
-    /**
-    * Smooth scrolling to the top of page
-    **/
-    $("html, body").animate({scrollTop : 0}, 1500);
-});
-
-// Smooth scrolling down
-$(".down").click(function() {
-    $('html, body').animate({
-        scrollTop: $(page).offset().top
-    }, 1500);
-});
+    
+})
