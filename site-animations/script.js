1bee410b92e8a055f7d2ee59af1876f5434c0e2d
Kathleen_M <kathleen.malialin@gmail.com>
2019-12-28 09:16:11 -0300

    add-txt

--- site-animations/script.js
+++ site-animations/script.js
@@ -1,11 +1,5 @@
 
      $(document).ready(function(){
-     var reponse = prompt("Bonjour, comment t'appelles-tu ?", "<Entrez ici votre prénom>");
-     if (reponse == null) {
-       alert("Vous avez cliqué sur Annuler");
-     } else {
-       alert("Bienvenue " + reponse + ", ça roule ?");
-     }
 
     $('.js-scrollTo').on('click', function() { // Au clic sur un élément
         var page = $(this).attr('href'); // Page cible
