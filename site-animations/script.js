afd506305f76921ea57fff509d52aee4357adaad
Kathleen_M <kathleen.malialin@gmail.com>
2019-12-28 10:32:35 -0300

    add-alert

--- site-animations/script.js
+++ site-animations/script.js
@@ -1,12 +1,12 @@
+$(document).ready(function () {
+  alert("Bienvenue ! Pour votre confort visuel, vous pouvez activer le mode nuit en cliquant sur la pastille en bas à droite de votre écran. Enjoy ! :)");
+  $('.js-scrollTo').on('click', function () { // Au clic sur un élément
+    var page = $(this).attr('href'); // Page cible
+    var speed = 1800; // Durée de l'animation (en ms)
+    $('html, body').animate({
+      scrollTop: $(page).offset().top
+    }, speed); // Go
+    return false;
+  });
 
-     $(document).ready(function(){
-
-    $('.js-scrollTo').on('click', function() { // Au clic sur un élément
-        var page = $(this).attr('href'); // Page cible
-        var speed = 1800; // Durée de l'animation (en ms)
-        $('html, body').animate( { scrollTop: $(page).offset().top }, speed ); // Go
-        return false;
-    });
-
-    
 })
