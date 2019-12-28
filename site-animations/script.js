1f6a6ca64b0ce280e04efa9b99b513ed95a3cf48
Kathleen_M <kathleen.malialin@gmail.com>
2019-12-27 22:45:16 -0300

    add-elements

---
+++ site-animations/script.js
@@ -0,0 +1,22 @@
+(document).ready(function() {
+    $('.js-scrollTo').on('click', function() { // Au clic sur un élément
+        var page = $(this).attr('href'); // Page cible
+        var speed = 1800; // Durée de l'animation (en ms)
+        $('html, body').animate( { scrollTop: $(page).offset().top }, speed ); // Go
+        return false;
+    });
+});
+
+$(function() {
+    /**
+    * Smooth scrolling to the top of page
+    **/
+    $("html, body").animate({scrollTop : 0}, 1500);
+});
+
+// Smooth scrolling down
+$(".down").click(function() {
+    $('html, body').animate({
+        scrollTop: $(page).offset().top
+    }, 1500);
+});
