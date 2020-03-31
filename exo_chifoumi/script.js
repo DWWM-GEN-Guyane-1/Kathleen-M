fcf8eafe95312536f63b45ddda5e0994c5bd49bd
Kath97 <kathleen.malialin@gmail.com>
2020-03-31 13:56:49 -0300

    add-function-newGame

--- exo_chifoumi/script.js
+++ exo_chifoumi/script.js
@@ -1,33 +1,21 @@
 var toi; //Nominate player 1
 var bot; // Nominate player 2
-
-var pierre = 1
-
-
 var pierrepPapierCiseaux = ["pierre", "papier", "ciseaux"]; //initialize array (game choice)
 var choixBot = pierrepPapierCiseaux[Math.floor(Math.random() * pierrepPapierCiseaux.length)]; //"Bot" Random game choice
 
-document.getElementById("btnRock").addEventListener("click", function () {
-    document.getElementById("newGame");
-}); //
-
-
 function newGame() {
-if (choixBot == ) {
-    return "Tu as gagné !"
-} else if (choixBot){
-    return "Tu as perdu"
-} else {
-    return "Match nul"
-}
-
-}
-//    var choixToi = document.getElementById("toiChoice").value; //"Toi" made a choice
-//    choixToi.onclick = showAlert;
+    if (choixBot == "pierre" && choixToi == "pierre") {
+        return "Match nul"
+    } else if (choixBot == "pierre" && choixToi == "papier") {
+        return "Tu as perdu !"
+    } else {
+        return "Tu as gagné !"
+    }
 };
 
-function showAlert() {
-    alert("tata");
-}
+
+// function showAlert() {
+//     alert("tata");
+// }
 
 //     if choixVous == choixBot alors = 0
